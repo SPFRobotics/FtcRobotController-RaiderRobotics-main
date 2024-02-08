9ba6e54370cf3ba13d87443544445aec5d323ca9
24ekrantz <elijkrantz@gmail.com>
2024-02-08 15:04:20 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoIntakeAidenOutOfWayRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoIntakeAidenOutOfWayRed.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -12,6 +13,7 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Autonomous
+@Disabled
 public class AutoIntakeAidenOutOfWayRed extends LinearOpMode {
     //MOVEMENT MOTOR VARS
     private static final double strafeMult = 1.2;
