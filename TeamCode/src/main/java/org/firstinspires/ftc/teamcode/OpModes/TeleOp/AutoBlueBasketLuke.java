d89e3772c44240d1472eb274cc0b6f7819bb8820
Luke Zimmerman <lukezimmerman2007@gmail.com>
2024-10-17 16:47:50 -0400

    Made Extendo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/AutoBlueBasketLuke.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/AutoBlueBasketLuke.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Hardware.Robot.Extendo;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.LinearSlide;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.MecanumChassis;
 import org.firstinspires.ftc.teamcode.Hardware.aprilTagDetectionMovement;
@@ -12,12 +13,14 @@ public class AutoBlueBasketLuke extends LinearOpMode {
     MecanumChassis chassis = new MecanumChassis(this);
     LinearSlide slide = new LinearSlide(this);
     aprilTagDetectionMovement aTag = new aprilTagDetectionMovement(this);
+    Extendo extendo = new Extendo(this);
 
     public void runOpMode(){
         chassis.initializeMovement();
         aTag.initCam2();
         aTag.camOn();
         slide.initSlides();
+        extendo.initSlides();
     }
 
 }
