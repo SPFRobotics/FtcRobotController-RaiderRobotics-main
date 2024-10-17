873202369b1e09ea384f71ca1604022a85df704f
Luke Zimmerman <lukezimmerman2007@gmail.com>
2024-10-17 16:32:48 -0400

    Updated Linear Slide Class to this year's design

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/AutoBlueBasketLuke.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Hardware.Robot.LinearSlide;
+import org.firstinspires.ftc.teamcode.Hardware.Robot.MecanumChassis;
+import org.firstinspires.ftc.teamcode.Hardware.aprilTagDetectionMovement;
+
+@Autonomous
+public class AutoBlueBasketLuke extends LinearOpMode {
+    MecanumChassis chassis = new MecanumChassis(this);
+    LinearSlide slide = new LinearSlide(this);
+    aprilTagDetectionMovement aTag = new aprilTagDetectionMovement(this);
+
+    public void runOpMode(){
+        chassis.initializeMovement();
+        aTag.initCam2();
+        aTag.camOn();
+        slide.initSlides();
+    }
+
+}
