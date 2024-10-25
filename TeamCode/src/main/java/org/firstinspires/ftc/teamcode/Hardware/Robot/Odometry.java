34e0c100ca7760281ff0cc0bf982de600698e9fb
Luke Zimmerman <lukezimmerman2007@gmail.com>
2024-10-25 15:31:26 -0400

    Odometry Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot/Odometry.java
@@ -46,13 +46,14 @@ public class Odometry {
         double deltaCenterPod = centerPod.getCurrentPosition() - prevEncoder[1];
         double deltaRightPod = rightPod.getCurrentPosition() - prevEncoder[2];
 
-        double phi = (ticksToCM(deltaLeftPod) - ticksToCM(deltaRightPod)) / TRACKWIDTH_CM;
+        double phi = (ticksToCM(deltaLeftPod) - ticksToCM(deltaRightPod)) / TRACKWIDTH_CM; // need units to be consistent for this calculation
         // see Gm0, phi (deltaHeading) is the  changes in right and left Pods divided by trackwidth, which we measured on the robot to be the constant TRACKWIDTH_cm
-        double deltaMiddlePos = (deltaRightPod + deltaLeftPod) / 2; // The change in position of the robots center is the average of changes on right and left pod
-        double deltaPerpPos = deltaCenterPod - OFFSET_CM; // See Gm0, our offset on 2024-25 Robot is 0, our pod is in the center
+        double deltaMiddlePos = (ticksToCM(deltaRightPod) + ticksToCM(deltaLeftPod)) / 2; // The change in position of the robots center is the average of changes on right and left pod
+        double deltaPerpPos = ticksToCM(deltaCenterPod) - OFFSET_CM; // See Gm0, our offset on 2024-25 Robot is 0, our pod is in the center
 
-        double deltaX = ticksToCM(deltaMiddlePos) * Math.cos(pose[2]) - ticksToCM(deltaPerpPos) * Math.sin(pose[2]); // See Gm0 for derivation
-        double deltaY = ticksToCM(deltaMiddlePos) * Math.sin(pose[2]) - ticksToCM(deltaPerpPos) * Math.cos(pose[2]); // See. G. m. 0.
+        double deltaX = deltaMiddlePos * Math.cos(pose[2]) - deltaPerpPos * Math.sin(pose[2]); // See Gm0 for derivation
+        double deltaY = deltaMiddlePos * Math.sin(pose[2]) - deltaPerpPos * Math.cos(pose[2]); // See. G. m. 0.
+        // deltaMiddlePos and deltaPerpPos are already in CM rather than motor ticks
 
         pose[0] += deltaX; // remember pose[0] is x position of robot
         pose[1] += deltaY; // remember pose[1] is y position of robot
