3b9988b54859b79e49a717c1ca1245b9c299b571
Luke Zimmerman <lukezimmerman2007@gmail.com>
2024-11-01 10:49:52 -0400

    Auto for all 4 positions, pending tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoBlueObsZoneLuke.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoBlueObsZoneLuke.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.Hardware.Robot.Extendo;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.LinearSlide;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.MecanumChassis;
 import org.firstinspires.ftc.teamcode.Hardware.aprilTagDetectionMovement;
-
+// START WITH ROBOT ON A3 WITH RIGHT WHEELS ON COORDINATE LINE
 @Autonomous
 public class AutoBlueObsZoneLuke extends LinearOpMode {
     MecanumChassis chassis = new MecanumChassis(this);
@@ -26,15 +26,16 @@ public class AutoBlueObsZoneLuke extends LinearOpMode {
         slide.initSlides();
         extendo.initSlides();
         claw.init();
-        slide.slide(19.5, .6);
-        chassis.move(.5, "forward", 81.28);
-        chassis.move(.5,"right",58.42);
-        slide.LiftHold();
-        slide.slide(-3,.6);
-        claw.open();
+        slide.slide(19.5, .6); // raises lift high enough to place specimen on chamber
+        chassis.move(.5, "forward", 81.28); // should move towards submersible
+        chassis.move(.5,"left",30); // should move towards submersible
+        slide.slide(-3,.6); // lowers lift (specimen should attach by now)
+        claw.open(); // let go of specimen
+        // next 2 move commands move towards parking zone
         chassis.move(.5, "backward", 75);
-        chassis.move(.5, "left", 154.94);
+        chassis.move(.5, "right", 100);
         slide.slide(-16.5, .6);
+        // measurements past here are guesswork, they have to be tested (SO DOES THE CLAW)
+        // once all measurements are finetuned this should place a pre-loaded specimen and park in the observation zone (13pts)
     }
-
 }
