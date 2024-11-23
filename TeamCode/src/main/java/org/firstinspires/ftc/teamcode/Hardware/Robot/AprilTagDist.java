e67801594e8e7b10f5e04322a2217b7624d8bf56
webwizardsaanvi <webwizardsaanvi@gmail.com>
2024-11-23 14:08:43 -0500

    things are happening :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot/AprilTagDist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot/AprilTagDist.java
@@ -5,38 +5,54 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 
 import android.util.Size;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-//hell0
-//hi
+
 
 public class AprilTagDist {
-    public double inToCm(double inches) { return inches * 2.54; }
-        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
-                .setDrawAxes(true)
-                .setDrawCubeProjection(true)
-                .setDrawTagID(true)
-                .setDrawTagOutline(true)
-                .build();
+    private LinearOpMode opmode = null;
 
-        VisionPortal visionPortal = new VisionPortal.Builder()
-                .addProcessor(tagProcessor)
-                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-                .setCameraResolution(new Size(640, 480))
-                .build();
-    public double getDist(){
+    public AprilTagDist(LinearOpMode lom) {
+        opmode = lom;
+    }
+
+    public double inToCm(double inches) {
+        return inches * 2.54;
+    }
+
+    AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+            .setDrawAxes(true)
+            .setDrawCubeProjection(true)
+            .setDrawTagID(true)
+            .setDrawTagOutline(true)
+            .build();
+
+    VisionPortal visionPortal = new VisionPortal.Builder()
+            .addProcessor(tagProcessor)
+            .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+            .setCameraResolution(new Size(640, 480))
+            .build();
+
+    public double getDist() {
         double dist = 0;
-            if (!tagProcessor.getDetections().isEmpty()){
-                AprilTagDetection tag = tagProcessor.getDetections().get(0);
-                double x = tag.ftcPose.x;
-                double y = tag.ftcPose.y;
-                dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-            }
+        if (!tagProcessor.getDetections().isEmpty()) {
+            AprilTagDetection tag = tagProcessor.getDetections().get(0);
+            double x = tag.ftcPose.x;
+            double y = tag.ftcPose.y;
+            dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        }
         return dist;
     }
+        public LinearOpMode getOpmode(){
+            return opmode;
+        }
+
+        public void setOpmode (LinearOpMode opmode){
+            this.opmode = opmode;
+        }
+    }
 }
-
-
-
