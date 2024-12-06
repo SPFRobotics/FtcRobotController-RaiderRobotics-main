41e154b553d653a579f56cbd43d32479e322fdbe
Mr.Spaceman <mrspaceman1000@gmail.com>
2024-12-06 00:04:30 -0500

    Adds Mechanium Drive to Outreach1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Outreach1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Outreach1.java
@@ -15,12 +15,36 @@ import org.firstinspires.ftc.teamcode.Hardware.Robot.Odometry;
 
 @TeleOp(name="Outreach1")
 public class Outreach1 extends LinearOpMode{
-    public void runOpMode(){
+    private DcMotor rightFront = null;
+    private DcMotor leftFront = null;
+    private DcMotor rightBack = null;
+    private DcMotor leftBack = null;
 
+    public void runOpMode(){
+        rightFront = hardwareMap.get(DcMotor.class, "Motor1");
+        leftFront = hardwareMap.get(DcMotor.class, "Motor0");
+        rightBack = hardwareMap.get(DcMotor.class, "Motor3");
+        leftBack = hardwareMap.get(DcMotor.class, "Motor2");
+
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
         while (opModeIsActive()){
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
 
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            rightFront.setPower((y - x - rx) / denominator);
+            leftFront.setPower((y + x + rx) / denominator);
+            rightBack.setPower((y + x - rx) / denominator);
+            leftBack.setPower((y - x + rx) / denominator);
         }
     }
 }
