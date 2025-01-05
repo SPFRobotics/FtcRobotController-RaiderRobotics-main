155175759308040d3accb146f45837571760ad9d
mrspaceman <mrspaceman1000@gmail.com>
2025-01-05 00:36:33 -0500

    Made button toggle function in ClawTesting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ClawTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ClawTesting.java
@@ -1,11 +1,46 @@
+//This is a class written for testing purposes only to control a new claw system
+//This is not to be used for the actual competition
 package org.firstinspires.ftc.teamcode.testing;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 @TeleOp(name = "ClawTesting")
 public class ClawTesting extends LinearOpMode{
+    //A static method that makes toggling a mode easy
+
+    private boolean ButtonToggle(Method button){
+        boolean toggleMode = false;
+        boolean stillPressed = false;
+        try{
+            if ((boolean) button.invoke(gamepad1) && !toggleMode && !stillPressed){
+                toggleMode = true;
+            }
+            if (toggleMode){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (IllegalAccessException e){
+
+        }
+        catch (IllegalArgumentException e){
+
+        }
+        catch (Exception e){
+
+        }
+        return false;
+    }
     private Servo RotationServo = null;
     private Servo WristServo = null;
     private Servo ClawRotationServo = null;
@@ -70,6 +105,18 @@ public class ClawTesting extends LinearOpMode{
                 toggleClaw = false;
                 stillPressed = true;
             }
+
+            //Telemetry
+            telemetry.addLine("==========================================");
+            telemetry.addLine(String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)));
+            telemetry.addLine("==========================================");
+            telemetry.addLine("Rotation Servo Pos: " + RotationServo.getPosition());
+            telemetry.addLine("Wrist Servo Pos: " + WristServo.getPosition());
+            telemetry.addLine("Claw Rotation Servo Pos: " + ClawRotationServo.getPosition());
+            telemetry.addLine("Claw Servo Pos: " + ClawServo.getPosition());
+            telemetry.addLine("==========================================");
+            telemetry.addLine(String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)) + String.valueOf((int)(Math.random() * 2)));
+            telemetry.addLine("==========================================");
 
         }
 
