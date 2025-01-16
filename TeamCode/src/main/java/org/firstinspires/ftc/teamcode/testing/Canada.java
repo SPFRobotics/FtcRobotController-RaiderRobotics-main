cd13a1d58a5ac3e1af62391d82dd3ffcea44da9b
ReverseFlash123 <149724395+ReverseFlash123@users.noreply.github.com>
2025-01-16 13:58:32 -0800

    Ara made Canada class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Canada.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Canada.java
@@ -10,10 +10,8 @@ public class Canada {
     private Servo ClawServo = null;
 
 
-
-
     // - rs is RotationServo, ws is WristServo, crs is ClawRotationServo, cs is ClawServo
-    public Canada(Servo rs, Servo ws, Servo crs, Servo cs){
+    public Canada(Servo rs, Servo ws, Servo crs, Servo cs) {
         RotationServo = rs;
         WristServo = ws;
         ClawRotationServo = crs;
@@ -26,31 +24,25 @@ public class Canada {
     }
 
     // - rrs is resetRotationServo, rws is restWristServo, rclrose is resetClawRotationServo, rclse is resetClawServo
-    public void resetClaw(Servo rrs, Servo rws, double rclrose, double rclse){
+    public void resetClaw(Servo rrs, Servo rws, double rclrose, double rclse) {
         RotationServo = rrs;
         WristServo = rws;
         ClawRotationServo.setPosition(rclrose);
         ClawServo.setPosition(rclse);
 
 
-
-
-
     }
 
     // - srs is setRotationServo, sws is setWristServo
-    public void setClaw(Servo srs, Servo sws){
+    public void setClaw(Servo srs, Servo sws) {
         RotationServo = srs;
         WristServo = sws;
     }
 
 
-
-    }
-
 //-------------------------------------------------------------------------------------------------------
 
-    public void closClaw(double closePos){
+    public void closClaw(double closePos) {
 
         closePos = 0;
         ClawServo.setPosition(closePos);
@@ -58,21 +50,23 @@ public class Canada {
     }
 
 
-    public void openClaw(double openPos){
+    public void openClaw(double openPos) {
 
         openPos = .6;
         ClawServo.setPosition(openPos);
 
     }
 
-    public void rotateClaw(double rotatePos){
+    public void rotateClaw(double rotatePos) {
 
         rotatePos = 0.5276;
         ClawRotationServo.setPosition(rotatePos);
     }
 
-
 }
+
+
+
 
 
 
