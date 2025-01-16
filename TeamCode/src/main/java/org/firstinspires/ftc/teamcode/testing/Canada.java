77553294f38b6ffd22d7c3c3a16f6f38f370f092
ReverseFlash123 <149724395+ReverseFlash123@users.noreply.github.com>
2025-01-16 13:16:05 -0800

    Ara made Canada class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Canada.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.testing;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Canada {
+    private Servo RotationServo = null;
+    private Servo WristServo = null;
+    private Servo ClawRotationServo = null;
+    private Servo ClawServo = null;
+
+
+
+
+    // - rs is RotationServo, ws is WristServo, crs is ClawRotationServo, cs is ClawServo
+    public Canada(Servo rs, Servo ws, Servo crs, Servo cs){
+        RotationServo = rs;
+        WristServo = ws;
+        ClawRotationServo = crs;
+        ClawServo = cs;
+
+
+
+
+
+    }
+
+
+
+
+}
