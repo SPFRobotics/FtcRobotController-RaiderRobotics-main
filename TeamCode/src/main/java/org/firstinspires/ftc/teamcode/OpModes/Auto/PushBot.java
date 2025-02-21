de68f5f7c17424f050234a970e6db2c8029ed724
manavshah1199 <manavshah0709@gmail.com>
2025-02-21 15:47:27 -0500

    Fixed MecanumChassis Added my own AUTO program that will be used for tele-op Made a prototype for distance sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PushBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PushBot.java
@@ -2,55 +2,64 @@ package org.firstinspires.ftc.teamcode.OpModes.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.Claw;
-import org.firstinspires.ftc.teamcode.Hardware.Robot.Extendo;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.LinearSlide;
 import org.firstinspires.ftc.teamcode.Hardware.Robot.MecanumChassis;
+
 import com.qualcomm.robotcore.hardware.IMU;
 
-@Autonomous
+@Autonomous(name = "PushBot Autonomous", group = "Autonomous")
 public class PushBot extends LinearOpMode {
-    public void runOpMode() throws InterruptedException{
-        MecanumChassis chassis = new MecanumChassis(this);
-        LinearSlide slide = new LinearSlide(this);
-        Claw claw = new Claw(this);
-        IMU imu = null;
 
+    private MecanumChassis chassis;
+    private LinearSlide lift;
+    private Claw claw;
+    private IMU imu;
 
-        chassis.initializeMovement();
-        slide.initSlides();
-        claw.init();
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        chassis = new MecanumChassis(this);
+        lift = new LinearSlide(this);
+        claw = new Claw(this);
         imu = hardwareMap.get(IMU.class, "imu");
+        imu.resetYaw();
+        chassis.initializeMovement();
+        claw.init();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
         waitForStart();
-
-        chassis.move(.5, "forward", 6.62);
-        chassis.rotate(90, .5);
-        chassis.move(.3, "left", 11.24);
-        slide.slide(30, .5);
-        chassis.move(.5, "forward", 48.52);
-        claw.open();
-        chassis.move(.5, "left", 1.20);
-        chassis.move(.5, "backward", 43.52);
-        slide.slide(-30, .5);
-        chassis.move(.5, "right", 122.02);
-        chassis.move(.5, "forward", 22.94);
-        chassis.move(.5, "left", 120.02);
-        chassis.move(.5, "right", 120.02);
-        chassis.move(.5, "forward", 20.4);
-        chassis.move(.5, "left", 112.02);
-        chassis.move(.5, "right", 120.02);
-        chassis.move(.5, "forward", 20.62);
-        chassis.move(.5, "left", 100.02);
+        if (opModeIsActive()) {
 
 
-        telemetry.addLine("Yaw: " + imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+            chassis.move(0.5, "forward", 6);
 
 
+            // Rotate 90 degrees
+            imu.resetYaw();
+            chassis.rotate(90, 0.5);
 
+            // Move backward 6 inches
+            chassis.move(0.5, "backward", 6);
 
+            // Test linear slide movement
+            lift.slide(30, 0.5);
+            sleep(500);
+            lift.slide(-30, 0.5);
 
+            // Open claw
+            claw.open();
 
+            // Display final yaw angle
+            YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
+            telemetry.addData("Final Yaw", angles.getYaw(AngleUnit.DEGREES));
+            telemetry.addData("Status", "Autonomous Complete");
+            telemetry.update();
+        }
     }
 }
+
+
