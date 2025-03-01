69c744bdfa21093047eae25ce4718a976bc165c5
webwizardsaanvi <webwizardsaanvi@gmail.com>
2025-03-01 13:01:47 -0500

    limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/limelighttest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/limelighttest.java
@@ -5,6 +5,7 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -12,6 +13,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 import java.util.List;
+@Autonomous
 public class limelighttest extends LinearOpMode {
 
     private Limelight3A limelight;
