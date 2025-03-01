0ca6995003285115db75acb282a22e76bf3d39ba
webwizardsaanvi <webwizardsaanvi@gmail.com>
2025-03-01 13:10:09 -0500

    limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/limelighttest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/limelighttest.java
@@ -29,6 +29,7 @@ public class limelighttest extends LinearOpMode {
         /*
          * Starts polling for data.
          */
+        waitForStart();
         limelight.start();
 
         while (opModeIsActive()) {
