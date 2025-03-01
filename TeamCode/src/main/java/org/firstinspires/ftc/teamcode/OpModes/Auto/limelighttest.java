dbec2ed02cbc9a1197369c75a4d8be9cf0ce3923
manavshah1199 <manavshah0709@gmail.com>
2025-03-01 15:23:39 -0500

    Fixed MecanumChassis Added my own AUTO program that will be used for tele-op Made a prototype for distance sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/limelighttest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/limelighttest.java
@@ -7,6 +7,8 @@ import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
