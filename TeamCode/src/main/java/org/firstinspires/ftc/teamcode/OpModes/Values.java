22b58b034b24cb655ef67f01d74314b276c6fbec
mrspaceman <mrspaceman1000@gmail.com>
2025-03-09 23:30:32 -0400

    Adds code for vertical linear slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Values.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Values.java
@@ -11,4 +11,14 @@ public class Values {
         public static double rOuttakeWrist = 0;
         public static double lOuttakeWrist = 0;
     }
+
+    @Config
+    public static class verticalSlide{
+        public static double power = 1;
+    }
+
+    @Config
+    public static class Testing{
+        public static int loopTo = 0;
+    }
 }
