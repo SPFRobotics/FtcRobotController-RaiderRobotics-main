27cc34b400aed3e39ee9d4708726cd6968b71248
mrspaceman <mrspaceman1000@gmail.com>
2025-03-15 20:10:17 -0400

    Gets rid of Axon encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Values.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Values.java
@@ -5,11 +5,11 @@ import com.acmerobotics.dashboard.config.Config;
 public class Values {
     @Config
     public static class Outtake{
-        public static double ClawClosedPos = 0.65;
-        public static double ClawOpenPos = 0;
-        public static double wristSpeedMultiplyer = 0.05;
-        public static double rOuttakeWrist = 0;
-        public static double lOuttakeWrist = 0;
+        public static double ClawClosedPos = 0;
+        public static double ClawOpenPos = 0.3;
+        public static double wristSpeedMultiplyer = 0.01;
+        public static double wristTransferPos = 0.371;
+        public static double wristIdlePos = 0;
     }
 
     @Config
@@ -22,12 +22,11 @@ public class Values {
         public static double ClawOpenPos = 0.5;
         public static double ClawClosedPos = 1;
         public static double wristLeftTransferPos = 0.05;
-        public static double wristLeftIntakePos = 0.33;
-        public static double wristRightTransferPos = 0.05;
+        public static double wristTransferPos = 0;
         public static double wristRightIntakePos = 0.33;
         public static double rotationIntakePos = 0;
         public static double rotationTransferPos = 0;
-        public static double wristSpeedMultiplyer = 0.1;
+        public static double wristSpeedMultiplyer = 0.01;
 
     }
 }
